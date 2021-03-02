26ea91da4ec541ba39c7ebb8ec58c869a4690805
Santosh K <san587dev@gmail.com>
2021-03-02 09:27:32 -0600

    updated the Zip and Send mail

--- src/main/java/mail/ZipAndSendMail.java
+++ src/main/java/mail/ZipAndSendMail.java
@@ -1,10 +1,45 @@
 package mail;
 
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Properties;
+
 public class ZipAndSendMail {
 
+    Properties emailProperties;
+    Session mailSession;
+    MimeMessage emailMessage;
 
-    public static void main(String args[]) {
-        String reportFolder = System.getProperty("user.dir")+"//reports//";
+    static String[] toEmails = {"san590dev@gmail.com", "san589dev@gmail.com"};
+    static String fromUser = "san588dev@gmail.com";
+    static String password = "";
+
+    public static void main(String args[]) throws Exception {
+        String reportFolder = System.getProperty("user.dir") + "//reports//";
+
+        File dir = new File(reportFolder);
+        File[] files = dir.listFiles();
+        File lastModified = Arrays.stream(files).filter(File::isDirectory).max(Comparator.comparing(File::lastModified)).orElse(null);
+        System.out.println(lastModified.getName());
+
+        Zip.zipDir(reportFolder + "//" + lastModified.getName(), reportFolder + "//" + lastModified.getName() + ".zip");
+
+        Mail javaEmail = new Mail();
+        javaEmail.setMailServerProperties();
+
+        javaEmail.createEmailMessage(
+                "Automation Test Reports",
+                "Please find the reports in attached",
+                reportFolder + "//" + lastModified.getName() + ".zip",
+                "Reports.zip",
+                toEmails
+
+        );
+        javaEmail.sendEmail(fromUser, password);
 
     }
 
