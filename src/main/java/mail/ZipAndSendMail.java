ba2d8ced5c06d9c6066dc3fe1a8a4147eb567033
Santosh K <san587dev@gmail.com>
2021-03-02 09:01:51 -0600

    Added mail package where we can send updated reports.

---
+++ src/main/java/mail/ZipAndSendMail.java
@@ -0,0 +1,11 @@
+package mail;
+
+public class ZipAndSendMail {
+
+
+    public static void main(String args[]) {
+        String reportFolder = System.getProperty("user.dir")+"//reports//";
+
+    }
+
+}
